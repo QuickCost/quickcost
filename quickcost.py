4d1f4499b09f73ab86ae3b0691d0a72c4b826012
Laura <99843414+laurasales@users.noreply.github.com>
2023-06-10 23:13:55 +0200

    Connect code with GUI

--- quickcost.py
+++ quickcost.py
@@ -1,4 +1,17 @@
+from hwx import inspire
+from hwx import gui
 from enum import Enum
+
+
+# Global Variables
+model = inspire.getModel()
+model_name = model.name
+model_parts = model.parts
+number_parts = len(model_parts)
+parts_vector = []
+parts_map = {}
+material_prices = {}
+
 
 # Classes
 class Currency(Enum):
@@ -80,18 +93,16 @@ class Part:
         return f'Name: {self.name}, Component: {self.component}, Volume: {self.volume}, Material: {self.material}, Production Cost: {self.production_cost}, Reusable: {self.reusable}'
 
 
-# Global Variables
-material_prices = {}
-parts_vector = []
-parts_map = {}
-
-
 # Functions
+def clean_data():
+    parts_vector.clear()
+    parts_map.clear()
+
 def update_material_prices():
     # Update the material_prices dictionary with the current prices of materials
-
     for m in Material:
         # Interaction with the API
+        ## TODO: set material prices from API
         if m == Material.Aluminium:
             material_prices[m] = 1
         elif m == Material.Brass:
@@ -169,6 +180,13 @@ def get_material_type(part_material: str) -> Material:
     elif part_material == "Zamak":
         return Material.Zamak
 
+def get_name_parts():
+    name_parts = ()
+    for index in range(number_parts):
+        part_name = (model_parts[index].name,)
+        name_parts += part_name
+    return name_parts
+
 def total_cost_part(p: Part) -> float:
     # Calculate the cost of the part
     comp = p.get_component()
@@ -195,35 +213,413 @@ def total_cost_model() -> float:
     return total_cost
 
 
-# Main Program
-update_material_prices()
+# GUI
+# Widgets
+verticalSpacer = gui.SpacerItem(20, 221, spacing = 'Vertical')
 
-model_name = str(input('Enter the name of the model: '))
-currency = str(input('Enter the currency: '))
-number_parts = int(input('Enter the total number of parts: '))
+horizontalSpacer_2 = gui.SpacerItem(40, 20, spacing = 'Horizontal')
 
-for index in range(number_parts):
-    print('Part', index + 1, ':')
+partLabel = gui.Label(
+    parent = None,
+    name = 'partLabel',
+    text = 'PART',
+    font = dict(size = 11, bold = True),
+)
+partLabel.SetAlignment(gui.hwui.ui.AlignCenter)
 
-    part_name = str(input('Enter the name of the part: '))
-    part_component = str(input('Enter the type of component: '))
-    component_type = get_component_type(part_component)
-    part_volume = int(input('Enter the volume: '))
-    part_material = str(input('Enter the name of the material: '))
-    material_type = get_material_type(part_material)
-    material_cost = int(input('Optional: Enter the price per ton of the material: '))
-    part_production_cost = int(input('Enter the production cost: '))
-    part_reusable = bool(input('Is it reusable?: '))
+partNameLabel = gui.Label(
+    parent = None,
+    name = 'partNameLabel',
+    text = 'Select part',
+)
+
+partName = gui.ComboBox(
+    parent = None,
+    name = 'partName',
+    values = get_name_parts(),
+)
+
+partComponentLabel = gui.Label(
+    parent = None,
+    name = 'partComponentLabel',
+    text = 'Component',
+)
+
+## TODO: set selected value with the component of the part
+partComponent = gui.ComboBox(
+    parent = None,
+    name = 'partComponent',
+    values = ('Core', 'Chiller', 'Riser', 'Sleeve', 'Overflow', 'Filter', )
+)
+
+partVolumeLabel = gui.Label(
+    parent = None,
+    name = 'partVolumeLabel',
+    text = 'Volume',
+)
+
+## TODO: set text with the volume of the part
+partVolume = gui.LineEdit(
+    parent = None,
+    name = 'partVolume',
+    text = '0',
+)
+
+partMaterialLabel = gui.Label(
+    parent = None,
+    name = 'partMaterialLabel',
+    text = 'Material',
+)
+
+## TODO: set selected value with the material of the part
+partMaterial = gui.ComboBox(
+    parent = None,
+    name = 'partMaterial',
+    values = ('Aluminium', 'Brass', 'Carbon_Steel', 'Cast_Iron', 'Cobalt', 'Copper', 'Gold', 'Magnesium', 'Nickel', 'Silver', 'Stainless', 'Steel', 'Tin', 'Titanium', 'Zamak', )
+)
+
+materialCostLabel = gui.Label(
+    parent = None,
+    name = 'materialCostLabel',
+    text = 'Material cost (€/mt)',
+)
+
+## TODO: set text with the cost of the material selected
+materialCost = gui.LineEdit(
+    parent = None,
+    name = 'materialCost',
+    text = '0',
+)
+
+productionCostLabel = gui.Label(
+    parent = None,
+    name = 'productionCostLabel',
+    text = 'Production cost',
+)
+
+productionCost = gui.LineEdit(
+    parent = None,
+    name = 'productionCost',
+    text = '0',
+)
+
+isReusableLabel = gui.Label(
+    parent = None,
+    name = 'isReusableLabel',
+    text = 'Is it reusable?',
+)
+
+isReusable = gui.CheckBox(
+    parent = None,
+    name = 'isReusable',
+    text = '',
+)
+
+def onClickResetValuesButton(event):
+  ## TODO: set default values on GUI
+  gui.tellUser("Hello World")
+  
+resetValuesButton = gui.PushButton(
+    parent = None,
+    name = 'resetValuesButton',
+    text = 'Reset values',
+    command = onClickResetValuesButton,
+)
+
+horizontalSpacer_4 = gui.SpacerItem(40, 20, spacing = 'Horizontal')
+
+partCostLabel = gui.Label(
+    parent = None,
+    name = 'partCostLabel',
+    text = 'Part cost',
+    font = dict(size = 9, bold = True),
+)
+
+## TODO: add to text the selected currency symbol
+partCost = gui.Label(
+    parent = None,
+    name = 'partCost',
+    text = '0',
+    font = dict(size = 9, bold = True),
+)
+partCost.SetAlignment(gui.hwui.ui.AlignRight|gui.hwui.ui.AlignTrailing|gui.hwui.ui.AlignVCenter)
+
+horizontalSpacer = gui.SpacerItem(40, 20, spacing = 'Horizontal')
+
+modelLabel = gui.Label(
+    parent = None,
+    name = 'modelLabel',
+    text = 'MODEL',
+    font = dict(size = 11, bold = True),
+)
+modelLabel.SetAlignment(gui.hwui.ui.AlignCenter)
+
+modelNameLabel = gui.Label(
+    parent = None,
+    name = 'modelNameLabel',
+    text = 'Model name',
+)
+
+modelName = gui.Label(
+    parent = None,
+    name = 'modelName',
+    text = model_name,
+)
+
+numberPartsLabel = gui.Label(
+    parent = None,
+    name = 'numberPartsLabel',
+    text = 'Number of parts',
+)
+
+numberParts = gui.Label(
+    parent = None,
+    name = 'numberParts',
+    text = number_parts,
+)
+
+currencyLabel = gui.Label(
+    parent = None,
+    name = 'currencyLabel',
+    text = 'Select currency',
+)
+
+currency = gui.ComboBox(
+    parent = None,
+    name = 'currency',
+    values = ('EUR (€)', 'USD ($)', 'GBP (£)', )
+)
+
+totalCostLabel = gui.Label(
+    parent = None,
+    name = 'totalCostLabel',
+    text = 'Total cost',
+    font = dict(size = 10, bold = True),
+)
+
+## TODO: add to text the selected currency symbol
+totalCost = gui.Label(
+    parent = None,
+    name = 'totalCost',
+    text = '0',
+    font = dict(size = 10, bold = True),
+)
+totalCost.SetAlignment(gui.hwui.ui.AlignLeading|gui.hwui.ui.AlignLeft|gui.hwui.ui.AlignVCenter)
+
+def init_data():
+    clean_data()
+    update_material_prices()
+    number_parts = 1
     
-    part = Part(part_name, component_type, part_volume, material_type, part_production_cost, part_reusable)
-    parts_vector.append(part)
-    parts_map[part_name] = (part)
+    for index in range(number_parts):
+        part_name = partName.value
+        part_component = partComponent.value
+        component_type = get_component_type(part_component)
+        part_volume = float(partVolume.value)
+        part_material = partMaterial.value
+        material_type = get_material_type(part_material)
+        material_cost = float(materialCost.value)
+        part_production_cost = float(productionCost.value)
+        part_reusable = isReusable.value
+    
+        part = Part(part_name, component_type, part_volume, material_type, part_production_cost, part_reusable)
+        parts_vector.append(part)
+        parts_map[part_name] = (part)
 
-    part_cost = total_cost_part(part)
+def calculate_costs():
+    init_data()
+    part = parts_map[partName.value]
+    totalPartCost = str(total_cost_part(part))
+    partCost.text = totalPartCost
+    totalCostModel = str(total_cost_model())
+    totalCost.text = totalCostModel
 
-    print('The cost of the part', part.get_name(), 'is:', part_cost, currency)
-    print('-' * 50)
+def onClickCalculateCostsButton(event):
+    calculate_costs()
+  
+calculateCostsButton = gui.PushButton(
+    parent = None,
+    name = 'calculateCostsButton',
+    text = 'Calculate costs',
+    command = onClickCalculateCostsButton,
+)
 
-total_cost = total_cost_model()
-print('The total cost of the model', model_name, 'is:', total_cost, currency)
-print('-' * 100)
+def onClickGenerateCostReportButton(event):
+  ## TODO: generate cost report and download it
+  gui.tellUser("Hello World")
+  
+generateCostReportButton = gui.PushButton(
+    parent = None,
+    name = 'generateCostReportButton',
+    text = 'Generate cost report',
+    command = onClickGenerateCostReportButton,
+)
+
+horizontalSpacer_3 = gui.SpacerItem(40, 20, spacing = 'Horizontal')
+
+verticalSpacer_1 = gui.SpacerItem(20, 220, spacing = 'Vertical')
+
+# Layouts
+partNameHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        partNameLabel,
+        partName,
+    )
+)
+
+partComponentHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        partComponentLabel,
+        partComponent,
+    )
+)
+
+partVolumeHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        partVolumeLabel,
+        partVolume,
+    )
+)
+
+partMaterialHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        partMaterialLabel,
+        partMaterial,
+    )
+)
+
+materialCostHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        materialCostLabel,
+        materialCost,
+    )
+)
+
+productionCostHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        productionCostLabel,
+        productionCost,
+    )
+)
+
+isReusableHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        isReusableLabel,
+        isReusable,
+    )
+)
+
+resetAndPartCostHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        resetValuesButton,
+        '<->',
+        partCostLabel,
+        partCost,
+    )
+)
+
+partVerticalLayout = gui.VBoxLayout(
+    spacing = 6,
+    children = (
+        partLabel,
+        partNameHorizontalLayout,
+        partComponentHorizontalLayout,
+        partVolumeHorizontalLayout,
+        partMaterialHorizontalLayout,
+        materialCostHorizontalLayout,
+        productionCostHorizontalLayout,
+        isReusableHorizontalLayout,
+        resetAndPartCostHorizontalLayout,
+    )
+)
+
+modelNameHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        modelNameLabel,
+        modelName,
+    )
+)
+
+numberPartsHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        numberPartsLabel,
+        numberParts,
+    )
+)
+
+currencyHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        currencyLabel,
+        currency,
+    )
+)
+
+totalCostHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        totalCostLabel,
+        totalCost,
+    )
+)
+
+costsButtonsHorizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        calculateCostsButton,
+        generateCostReportButton,
+    )
+)
+
+modelVerticalLayout = gui.VBoxLayout(
+    spacing = 6,
+    children = (
+        modelLabel,
+        modelNameHorizontalLayout,
+        numberPartsHorizontalLayout,
+        currencyHorizontalLayout,
+        totalCostHorizontalLayout,
+        costsButtonsHorizontalLayout,
+    )
+)
+
+horizontalLayout = gui.HBoxLayout(
+    spacing = 6,
+    children = (
+        '<->',
+        partVerticalLayout,
+        '<->',
+        modelVerticalLayout,
+        '<->',
+    )
+)
+
+verticalLayout = gui.VBoxLayout(
+    spacing = 6,
+    margin = 9,
+    children = (
+        '<->',
+        horizontalLayout,
+        '<->',
+    )
+)
+
+# Dialog
+QuickCost = gui.Dialog(caption = 'QuickCost'
+, children = (verticalLayout,)
+)
+
+
+# Open QuickCost
+calculate_costs()
+show(QuickCost)
